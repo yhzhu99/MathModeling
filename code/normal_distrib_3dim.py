362aa6d3af50ae5f9f8ebddaccfb4065a6ecf240
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-02-08 22:08:33 +0800

    feat: intro

---
+++ code/normal_distrib_3dim.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+from mpl_toolkits.mplot3d import Axes3D
+
+#Parameters to set
+mu_x = 0
+variance_x = 3
+mu_y = 0
+variance_y = 15
+
+#Create grid and multivariate normal
+x = np.linspace(-10,10,500)
+y = np.linspace(-10,10,500)
+X, Y = np.meshgrid(x,y)
+pos = np.empty(X.shape + (2,))
+pos[:, :, 0] = X; pos[:, :, 1] = Y
+rv = multivariate_normal([mu_x, mu_y], [[variance_x, 0], [0, variance_y]])
+
+#Make a 3D plot
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot_surface(X, Y, rv.pdf(pos),cmap='viridis',linewidth=0)
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+plt.show()
